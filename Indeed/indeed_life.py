8485397cebb6e8c71912704bc1643c50c19ce0f2
mooshkid <mooshkid@gmail.com>
2022-03-17 17:55:53 +0900

    Updated

--- Indeed/indeed_life.py
+++ Indeed/indeed_life.py
@@ -24,17 +24,20 @@ start = time.time()
 #start loop (64 posts)
 table = driver.find_element(By.ID, 'cdjobstab')
 for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
-    for ahref in row.find_elements(By.CSS_SELECTOR, 'td:nth-child(2) > a'):
-        print('Starting(' + str(count) + '):' + ahref.text)
+    for aTag in row.find_elements(By.CSS_SELECTOR, 'td:nth-child(2) > a'):
+        #location name
+        for location in row.find_elements(By.CSS_SELECTOR, 'td:nth-child(3)'):
+            locText = location.text
+        print('Starting(' + str(count) + '): ' + aTag.text + ' - ' + locText)
         count +=1
 
         #open post in new tab
-        ahref.send_keys(Keys.CONTROL + Keys.ENTER)
+        aTag.send_keys(Keys.CONTROL + Keys.ENTER)
         #switch to tab1
         driver.switch_to.window(driver.window_handles[1])
 
         #status button
-        time.sleep(5)
+        time.sleep(4)
         statusButton = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="downshift-0-toggle-button"]')))
         #close job
         statusButton.send_keys(Keys.ENTER + Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.ENTER)
