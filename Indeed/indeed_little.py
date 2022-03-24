5e3114142b9c45326b2e05d07c2b01505f4b6f3c
mooshkid <mooshkid@gmail.com>
2022-03-24 14:59:25 +0900

    Updated

--- Indeed/indeed_little.py
+++ Indeed/indeed_little.py
@@ -7,7 +7,7 @@ import time
 import sys
 import logging
 
-#logging
+#logging config
 logging.basicConfig(
     level=logging.INFO,
     format=u'%(asctime)s [%(levelname)s] %(message)s',
@@ -16,21 +16,13 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
-
 class LoggerWriter:
     def __init__(self, level):
-        # self.level is really like using log.debug(message)
-        # at least in my case
         self.level = level
-
     def write(self, message):
-        # if statement reduces the amount of newlines that are
-        # printed to the logger
         if message != '\n':
             self.level(message)
-
     def flush(self): pass
-
 log = logging.getLogger(__name__)
 sys.stdout = LoggerWriter(log.debug)
 sys.stderr = LoggerWriter(log.error)
@@ -44,50 +36,58 @@ driver = webdriver.Chrome(options=options)
 #maximize browser
 driver.maximize_window()
 #launch URL
-driver.get("https://employers.indeed.com/j#cdjobs")
-time.sleep(5)
+driver.get("https://employers.indeed.com/j#cdjobs") #tab for copying
+driver.execute_script("window.open('https://employers.indeed.com/j#cdjobs');") #tab for closing
+driver.switch_to.window(driver.window_handles[1])
+time.sleep(6)
 
 #loop counter
 count = 0
 #start timer
 start = time.time()
+#create empty list
+theList = []
 
-#find elements
-table = driver.find_element(By.ID, 'cdjobstab')
-rowCount = table.find_elements(By.CSS_SELECTOR, 'tr')
-log.info(str(int(len(rowCount)) - 1) + ' Job Listings Found')
+#find number of open jobs
+rows = driver.find_elements(By.XPATH, '//*[@id="cdjobstab"]/div[3]/div/table/tbody/tr')
+jobCount = len(rows)
+log.info(str(jobCount) + ' Open Job Listings Found')
+#print list of all open jobs
+for row in rows:
+    for ahref in row.find_elements(By.CSS_SELECTOR, 'td:nth-child(2) > a'):
+        jobTitle = ahref.text
+        theList.append(jobTitle)
+log.info(theList)
 
-#start loop
-for rows in table.find_elements(By.CSS_SELECTOR, 'tr'):
-    for ahref in rows.find_elements(By.CSS_SELECTOR, 'td:nth-child(2) > a'):
+#CLOSE all open jobs
+log.info('Closing All Jobs')
+selectAll = driver.find_element(By.XPATH, '//*[@id="cdjobstab"]/div[3]/div/table/thead/tr/th[1]/label').click()
+changeStatusButton = driver.find_element(By.XPATH, '//*[@id="BulkUpdateStatusMenuButton"]').click()
+closeAll = driver.find_element(By.XPATH, '//*[@id="option-2--menu--2"]')
+closeAll.click()
+time.sleep(1)
+
+driver.switch_to.alert.accept() #driver.switch_to.alert.dismiss()
+log.info('Closed All Jobs')
+time.sleep(1)
+
+#close & switch tab
+driver.close()
+driver.switch_to.window(driver.window_handles[0])
+
+#START Copying
+for row in rows:
+    for ahref in row.find_elements(By.CSS_SELECTOR, 'td:nth-child(2) > a'):
+        jobTitle = ahref.text
         count += 1
-        log.info('Starting(' + str(count) + '): ' + ahref.text)
-
-        #open post in new tab
-        ahref.send_keys(Keys.CONTROL + Keys.ENTER)
-        #switch to tab1
-        driver.switch_to.window(driver.window_handles[1])
-
-        #status button
-        time.sleep(5)
-        statusButton = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="downshift-0-toggle-button"]')))
-        #close job
-        statusButton.send_keys(Keys.ENTER + Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.ENTER)
-        print('Closed')
-
-        #close & switch tab
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        time.sleep(1)
-
-
-    for label in rows.find_elements(By.CSS_SELECTOR, 'td > label'):
+        log.info('Starting(' + str(count) + '): ' + jobTitle)
+    for label in row.find_elements(By.CSS_SELECTOR, 'td > label'):
         label.click()
 
         #copy button
         copyButton = driver.find_element(By.XPATH, '//*[@id="cdjobstab"]/div[3]/div/div/div[2]/a[2]')
         copyButton.send_keys(Keys.CONTROL + Keys.ENTER)
-        print('Copying...')
+        log.info('Copying...')
         #uncheck box
         label.click()
         #switch to tab1
@@ -101,12 +101,11 @@ for rows in table.find_elements(By.CSS_SELECTOR, 'tr'):
         #don't optimize
         noOptimize = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="uniqueId1"]')))
         noOptimize.click()
-        print('Finished')
+        log.info('Finished.')
 
         #close & switch tab
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
-
 
 #print elapsed time
 end = time.time()
