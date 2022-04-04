b10445270e69965e26713e392d788fa1eb9225b7
mooshkid <mooshkid@gmail.com>
2022-04-04 15:48:33 +0900

    Added Loggings

--- Indeed/indeed_life.py
+++ Indeed/indeed_life.py
@@ -3,7 +3,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 import time
+import sys
+import logging
+
+#logging config
+path = os.getcwd()
+logPath = os.path.join(path, "logs/indeed_life.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=u'%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler(logPath, encoding='utf8'),
+        logging.StreamHandler()
+    ]
+)
+class LoggerWriter:
+    def __init__(self, level):
+        self.level = level
+    def write(self, message):
+        if message != '\n':
+            self.level(message)
+    def flush(self): pass
+log = logging.getLogger(__name__)
+sys.stdout = LoggerWriter(log.debug)
+sys.stderr = LoggerWriter(log.error)
 
 #chrome options
 options = webdriver.ChromeOptions()
@@ -28,7 +54,7 @@ for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
         #location name
         for location in row.find_elements(By.CSS_SELECTOR, 'td:nth-child(3)'):
             locText = location.text
-        print('Starting(' + str(count) + '): ' + aTag.text + ' - ' + locText)
+        log.info('Starting(' + str(count) + '): ' + aTag.text + ' - ' + locText)
         count +=1
 
         #open post in new tab
@@ -41,7 +67,7 @@ for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
         statusButton = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="downshift-0-toggle-button"]')))
         #close job
         statusButton.send_keys(Keys.ENTER + Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.ENTER)
-        print('Closed')
+        log.info('Closed')
 
         #close & switch tab
         driver.close()
@@ -55,7 +81,7 @@ for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
         #copy button
         copyButton = driver.find_element(By.XPATH, '//*[@id="cdjobstab"]/div[3]/div/div/div[2]/a[2]')
         copyButton.send_keys(Keys.CONTROL + Keys.ENTER)
-        print('Copying...')
+        log.info('Copying...')
         #uncheck box
         label.click()
         #switch to tab1
@@ -69,7 +95,7 @@ for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
         #don't optimize
         noOptimize = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="uniqueId1"]')))
         noOptimize.click()
-        print('Finished')
+        log.info('Finished')
 
         #close & switch tab
         driver.close()
@@ -78,6 +104,6 @@ for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
 #print elapsed time
 end = time.time()
 elapsed = end - start
-print('Task Completed in: ' + time.strftime('%H:%M:%S', time.gmtime(elapsed)))
+log.info('Task Completed in: ' + time.strftime('%H:%M:%S', time.gmtime(elapsed)))
 
 driver.close()
